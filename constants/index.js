3bc2df3c6547e7c9eecb9394276a6c291448d1a4
Ikuol <damienssopolo@gmail.com>
2023-05-26 18:59:27 +0000

    made some progress

--- constants/index.js
+++ constants/index.js
@@ -1,41 +1,101 @@
 export const datas = [
-    {
-        id:1,
-        title:"Seamless Collaboration",
-        text: "Easily collaborate with team members\non projects and ideas in real-time, boosting\nproductivity and teamwork"
-    },
-    {
-        id:2,
-        title:"Multi-Channel Support",
-        text: "Easily collaborate with team members\non projects and ideas in real-time, boosting\nproductivity and teamwork"
-    },
-    {
-        id:3,
-        title:"Advanced AI Capabilities",
-        text: "Easily collaborate with team members\non projects and ideas in real-time, boosting\nproductivity and teamwork"
-    },
-    {
-        id:4,
-        title:"Another Support",
-        text: "Easily collaborate with team members\non projects and ideas in real-time, boosting\nproductivity and teamwork"
-    },
-]
+  {
+    id: 1,
+    title: "Seamless Collaboration",
+    text: "Easily collaborate with team members\non projects and ideas in real-time, boosting\nproductivity and teamwork",
+  },
+  {
+    id: 2,
+    title: "Multi-Channel Support",
+    text: "Easily collaborate with team members\non projects and ideas in real-time, boosting\nproductivity and teamwork",
+  },
+  {
+    id: 3,
+    title: "Advanced AI Capabilities",
+    text: "Easily collaborate with team members\non projects and ideas in real-time, boosting\nproductivity and teamwork",
+  },
+  {
+    id: 4,
+    title: "Another Support",
+    text: "Easily collaborate with team members\non projects and ideas in real-time, boosting\nproductivity and teamwork",
+  },
+];
 
 export const inclusions = [
-    {
-        id:1,
-        text:"Use OmniGPT from your\nWhatsApp"
-    },
-    {
-        id:2,
-        text:"Transcribe speech into text"
-    },
-    {
-        id:3,
-        text:"Download your chat\nconversations"
-    },
-    {
-        id:4,
-        text:"Upload documents to get better insights"
-    },
-]
+  {
+    id: 1,
+    text: "Use OmniGPT from your\nWhatsApp",
+  },
+  {
+    id: 2,
+    text: "Transcribe speech into text",
+  },
+  {
+    id: 3,
+    text: "Download your chat\nconversations",
+  },
+  {
+    id: 4,
+    text: "Upload documents to get better insights",
+  },
+];
+
+export const integrations = [
+  {
+    id: 1,
+    imgUrl: "/whatsappimg.png",
+    title: "WhatsApp",
+    description:
+      "Connect your WhatsApp account to easily\nsend and receive messages directly with\nOmniGPT.",
+  },
+  {
+    id: 2,
+    imgUrl: "/slackk.png",
+    title: "Slack",
+    description:
+      "Connect your OmniGPT account to Slack\nand start collaborating with your team right\naway.",
+  },
+  {
+    id: 3,
+    imgUrl: "/chrome.png",
+    title: "Chrome Extension",
+    description:
+      "Install the OmniGPT extension on your\nChrome browser to access our AI right from\nyour browser window.",
+  },
+  {
+    id: 4,
+    imgUrl: "/line.png",
+    title: "Line",
+    description: "",
+  },
+  {
+    id: 5,
+    imgUrl: "/trello.png",
+    title: "Trello",
+    description: "",
+  },
+  {
+    id: 6,
+    imgUrl: "/jira.png",
+    title: "Jira",
+    description: "",
+  },
+  {
+    id: 7,
+    imgUrl: "/google.png",
+    title: "Google Workspace",
+    description: "",
+  },
+  {
+    id: 8,
+    imgUrl: "/github.png",
+    title: "GitHub",
+    description: "",
+  },
+  {
+    id: 9,
+    imgUrl: "/notion.png",
+    title: "Notion",
+    description: "",
+  },
+];
--- sections/Integrations.jsx
+++ sections/Integrations.jsx
@@ -1,20 +1,30 @@
-'use client';
+"use client";
 
 import styles from "@/styles";
+import { integrations } from "@/constants";
+import DisplayIntegration from "@/components/DisplayIntegration";
 
 const Integrations = () => {
   return (
     <section className={`${styles.xPaddings} py-12 relative`}>
-        <div className="container mx-auto flex flex-col
-        items-center justify-center gap-y-10">
-            <h2 className="text-white text-6xl font-semibold">
-                <span className="text-green">Integrations </span>
-                to Boost<br />
-                Your Workflow
-            </h2>
+      <div
+        className="container mx-auto flex flex-col
+        items-center justify-center gap-y-10"
+      >
+        <h2 className="text-white text-6xl font-semibold">
+          <span className="text-green">Integrations </span>
+          to Boost
+          <br />
+          Your Workflow
+        </h2>
+        <div className="grid grid-cols-3 gap-6">
+          {integrations.map((integration) => (
+            <DisplayIntegration key={integration.id} {...integration} />
+          ))}
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Integrations
+export default Integrations;
